4a1af949f09fd88ec8ce21066229766b40842e53
Влад Капуста <misiulavlad2@gmail.com>
2025-02-22 11:41:10 +0300

    add fourth task

---
+++ Project4/logic.cpp
@@ -0,0 +1,29 @@
+#include "util.h"
+void init_rand(int matrix[N][N]) {
+	srand(time(0));
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			matrix[i][j] = rand() % 101;
+		}
+	}
+}
+
+void print_matrix(int matrix[N][N]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int sum(int matrix[N][N]) {
+	int sum = 0;
+	for (int i = 1; i < N; i++) {
+		for (int j = 0; j < N - i - 1; j++) {
+			sum += matrix[i][j];
+		}
+
+	}
+	return sum;
+}
---
+++ Project4/util.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#define N 5
+void init_rand(int matrix[N][N]);
+void print_matrix(int matrix[N][N]);
+int sum(int matrix[N][N]);
